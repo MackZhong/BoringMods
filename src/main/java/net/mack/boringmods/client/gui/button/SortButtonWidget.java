f6d0520b604fa2ec6d404b879041909c8fd43b70
Mackzxh <mackz@sina.com>
2019-02-16 01:08:20 +0800

    1.14 19w07a.6, 0.3.6.107, 0.2.0.94

--- src/main/java/net/mack/boringmods/client/gui/button/SortButtonWidget.java
+++ src/main/java/net/mack/boringmods/client/gui/button/SortButtonWidget.java
@@ -4,8 +4,13 @@ import com.google.common.collect.SortedSetMultimap;
 import com.mojang.blaze3d.platform.GlStateManager;
 import javafx.collections.transformation.SortedList;
 import net.mack.boringmods.impl.ModInitializer;
+import net.mack.boringmods.mixin.MixinPlayerInventoryScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.FontRenderer;
+import net.minecraft.client.gui.ContainerScreen;
+import net.minecraft.client.gui.Screen;
 import net.minecraft.client.gui.ingame.PlayerInventoryScreen;
+import net.minecraft.client.gui.recipebook.RecipeBookGui;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.container.Container;
 import net.minecraft.container.PlayerContainer;
@@ -13,88 +18,113 @@ import net.minecraft.container.Slot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 
 import java.util.*;
 
 public class SortButtonWidget extends ButtonWidget {
     private org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger("boringmods");
 
-    private final Identifier texture;
-    private final int u;
-    private final int v;
-    private final int hoverVOffset;
     private final Container container;
-    private boolean pressed;
-    private int screenLeft;
-    private int screenTop;
+    private static boolean pressed = false;
+    private RecipeBookGui recipeBookGui;
 
-    public SortButtonWidget(int sId, int left, int top, int width, int height, int texU, int texV, int hoverOffset, Identifier tex,
-                            Container cont, int screenLeft, int screenTop) {
+    public SortButtonWidget(int sId, int left, int top, int width, int height,
+                            Container cont, RecipeBookGui recipe) {
         super(sId, left, top, width, height, "Sort");
 
-        this.u = texU;
-        this.v = texV;
-        this.hoverVOffset = hoverOffset;
-        this.texture = tex;
         this.container = cont;
-        this.screenLeft = screenLeft;
-        this.screenTop = screenTop;
+        this.recipeBookGui = recipe;
     }
 
-    public void setPos(int int_1, int int_2) {
-        this.x = int_1;
-        this.y = int_2;
-    }
-
-    public void draw(int int_1, int int_2, float float_1) {
-        this.hovered = int_1 >= this.x && int_2 >= this.y && int_1 < this.x + this.width && int_2 < this.y + this.height;
-        MinecraftClient minecraftClient_1 = MinecraftClient.getInstance();
-        minecraftClient_1.getTextureManager().bindTexture(this.texture);
-        GlStateManager.disableDepthTest();
-        int int_3 = this.v;
-        if (this.hovered) {
-            int_3 += this.hoverVOffset;
+    private int getLeft(boolean narrow, int screenWidth) {
+        int left;
+        if (this.recipeBookGui.isOpen() && !narrow) {
+            left = 177 + (screenWidth - 376) / 2;
+        } else {
+            left = (screenWidth - 176) / 2;
         }
 
-        this.drawTexturedRect(this.x, this.y, this.u, int_3, this.width, this.height);
+        return left;
+    }
 
-        if (this.pressed) {
+    public void draw(int cursorX, int cursorY, float float_1) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        FontRenderer fontRenderer = client.fontRenderer;
+        client.getTextureManager().bindTexture(WIDGET_TEX);
+        GlStateManager.color4f(1.0F, 1.0F, 1.0F, this.field_17766);
+        GlStateManager.enableBlend();
+        GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+
+
+//        logger.info(String.format("Window Width %d, Widnow Scaled Width %d", client.window.getWidth(), client.window.getScaledWidth()));
+
+        int left = this.getLeft(false, client.window.getScaledWidth());
+        this.x = left + 144;
+
+        int leftWidth = this.width / 2;
+        int rightWidth = this.width - leftWidth;
+        int topHeight = this.height / 2;
+        int buttomHeight = this.height - topHeight;
+        int textureId = this.getTextureId(this.isHovered());
+        int texX = 0;
+        int texY = 46 + textureId * 20;
+        this.drawTexturedRect(this.x, this.y, texX, texY, leftWidth, topHeight);
+        this.drawTexturedRect(this.x + leftWidth, this.y, 200 - rightWidth, texY, rightWidth, topHeight);
+        this.drawTexturedRect(this.x, this.y + topHeight, texX, texY + 20 - buttomHeight, leftWidth, buttomHeight);
+        this.drawTexturedRect(this.x + leftWidth, this.y + topHeight, 200 - rightWidth, texY + 20 - buttomHeight, rightWidth, buttomHeight);
+        this.drawBackground(client, cursorX, cursorY);
+        int fontColor = 14737632;
+        if (!this.enabled) {
+            fontColor = 10526880;
+        } else if (this.isHovered()) {
+            fontColor = 16777120;
+        }
+
+        this.drawStringCentered(fontRenderer, this.method_18329(), this.x + leftWidth, this.y + topHeight - fontRenderer.fontHeight / 2, fontColor | MathHelper.ceil(this.field_17766 * 255.0F) << 24);
+
+        int top = this.y - 62;
+//        left *= client.window.getScaleFactor();
+        if (SortButtonWidget.pressed) {
+            GlStateManager.disableDepthTest();
             for (int i = 0; i < this.container.slotList.size(); i++) {
                 Slot slot = this.container.slotList.get(i);
                 String text = String.format("%d", i);
-                minecraftClient_1.fontRenderer.drawWithShadow(text, slot.xPosition + this.screenLeft, slot.yPosition + this.screenTop, 0xffaabbcc);
+                client.fontRenderer.drawWithShadow(text, slot.xPosition + left, slot.yPosition + top, 0xffaabbcc);
             }
+            GlStateManager.enableDepthTest();
         }
-        GlStateManager.enableDepthTest();
     }
 
     public void onPressed(double x, double y) {
-        this.pressed = true;
-        List<ItemStack> stacks =  new ArrayList<>();//this.container.getStacks();
-//        logger.info(String.format("Pressed, %d stacks", stacks.size()));
-//        List<Slot> slots = new ArrayList<>();
-        for (int i = 9; i < Math.min(this.container.slotList.size(), 36); ++i) {
-            Slot slot = this.container.getSlot(i);
-            if(slot.hasStack())
-                stacks.add(slot.getStack());
-        }
-        stacks.sort(new Comparator<ItemStack>() {
-            @Override
-            public int compare(ItemStack o1, ItemStack o2) {
-                return o1.getDisplayName().getFormattedText().compareToIgnoreCase(o2.getDisplayName().getFormattedText());
-            }
-        });
-        for (int i = 9; i < Math.min(stacks.size(), 36); ++i) {
-            this.container.setStackInSlot(i, stacks.get(i - 9));
-        }
-
+        SortButtonWidget.pressed = !SortButtonWidget.pressed;
+        logger.info(SortButtonWidget.pressed ? "Pressed." : "UnPressed.");
+//        List<ItemStack> stacks =  new ArrayList<>();//this.container.getStacks();
+////        logger.info(String.format("Pressed, %d stacks", stacks.size()));
+////        List<Slot> slots = new ArrayList<>();
+//        for (int i = 9; i < Math.min(this.container.slotList.size(), 36); ++i) {
+//            Slot slot = this.container.getSlot(i);
+//            if(slot.hasStack())
+//                stacks.add(slot.getStack());
+//        }
+//        stacks.sort(new Comparator<ItemStack>() {
+//            @Override
+//            public int compare(ItemStack o1, ItemStack o2) {
+//                return o1.getDisplayName().getFormattedText().compareToIgnoreCase(o2.getDisplayName().getFormattedText());
+//            }
+//        });
+//        for (int i = 9; i < Math.min(stacks.size(), 36); ++i) {
+//            this.container.setStackInSlot(i, stacks.get(i - 9));
+//        }
 //
-//        this.container.updateSlotStacks(stacks);
-        this.container.sendContentUpdates();
+////
+////        this.container.updateSlotStacks(stacks);
+//        this.container.sendContentUpdates();
     }
 
-    public void onReleased(double x, double y){
-        logger.info("Released");
-        this.pressed = false;
+    public void onReleased(double x, double y) {
+        logger.info("Released.");
+        SortButtonWidget.pressed = false;
     }
 }
