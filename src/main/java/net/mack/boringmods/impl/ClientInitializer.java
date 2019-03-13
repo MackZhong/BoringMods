beea88779c3e9734f6108fdee7584103d7e977ea
Mack <mackzxh@gmail.com>
2019-03-13 13:15:42 +0800

    19w09a.8/0.3.7.109/0.2.3.109

--- src/main/java/net/mack/boringmods/impl/ClientInitializer.java
+++ src/main/java/net/mack/boringmods/impl/ClientInitializer.java
@@ -1,11 +1,26 @@
 package net.mack.boringmods.impl;
 
 
+import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
+import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+
 public class ClientInitializer implements net.fabricmc.api.ClientModInitializer {
     private org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger("boringmods");
+    public static FabricKeyBinding keybind;
 
     @Override
     public void onInitializeClient() {
+        KeyBindingRegistry.INSTANCE.addCategory("boringmods.category");
+        keybind = FabricKeyBinding.Builder.create(
+                new Identifier("boringmods:excavate"),
+                InputUtil.Type.KEY_KEYBOARD,
+                96,
+                "boringmods.category"
+        ).build();
+        KeyBindingRegistry.INSTANCE.register(keybind);
+
         logger.info("Boring Mods Client Initialization.");
     }
 }
